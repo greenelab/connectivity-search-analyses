cbacf1a99451617d8239017f4154632104f4cf6c
Kyle Kloster <kyle.kloster@gmail.com>
2017-05-22 18:01:43 -0400

    Cleaned up comparison of new method vs cypher, hetio in ipynb. Debugged path_county variable copying. Now flake8 compliant

--- hetmech/path_count.py
+++ hetmech/path_count.py
@@ -1,4 +1,3 @@
-import numpy
 import collections
 from scipy.sparse import csr_matrix
 import scipy.sparse
@@ -10,21 +9,22 @@ from .degree_weight import dwwc_step
 def dwpc(graph, metapath, damping=1.0, verbose=False):
     """
     Compute the degree-weighted path count (DWPC).
-    First checks that no more than one metanode type is repeated.
+    First checks that no more than one metanode type is repeated,
+    then separates metapath into three segments: head, loop, tail.
+    The 'loop' segment contains all instances of repeated nodes;
+    the three segments are handled with different multiplication rules.
     """
     print("Calling DWPC")
     dwpc_matrix = None
     # First read through sequence of metanode types
     nodetype_sequence = collections.defaultdict(int)
     for metaedge in metapath:
-        # metanodes = metaedge.get_nodes()
         nodetype_sequence[metaedge.source] += 1
         nodetype_sequence[metaedge.target] += 1
     # Identify the repeated node type
     # and check that no more than 1 metanodetype is repeated
-    repeated_node = filter(lambda x: nodetype_sequence[x] > 2,
-                           nodetype_sequence)
-    repeated_node = list(repeated_node)
+    repeated_node = list(filter(lambda x: nodetype_sequence[x] > 2,
+                         nodetype_sequence))
     if len(repeated_node) > 1:
         if verbose:
             print("Input metapath repeats more than one nodetype")
@@ -38,15 +38,16 @@ def dwpc(graph, metapath, damping=1.0, verbose=False):
             adj_mat = metaedge_to_adjacency_matrix(graph, metaedge)
             adj_mat = dwwc_step(adj_mat, damping, damping)
             if dwpc_matrix is None:
-                dwpc_matrix = csr_matrix(adj_mat)
+                dwpc_matrix = csr_matrix(adj_mat).copy()
             else:
                 dwpc_matrix = dwpc_matrix @ adj_mat
     else:
         # Determine start/endpoints for left,loop,right
         if verbose:
             print("Input metapath repeats exactly one nodetype")
-            print("Repeated node list: {}, type {}".format(repeated_node, repeated_node[0]))
-        
+            print("Repeated node list: {}, type {}"
+                  "".format(repeated_node, repeated_node[0]))
+
         first_appearance = None
         last_appearance = None
         for idx, metaedge in enumerate(metapath):
@@ -57,54 +58,60 @@ def dwpc(graph, metapath, damping=1.0, verbose=False):
                 last_appearance = idx
         if verbose:
             print("metapath has {} nodes, and {} are the repeated ones"
-                  "".format(len(metapath),[first_appearance, last_appearance]))
+                  "".format(len(metapath),
+                            [first_appearance, last_appearance]))
 
         # Handle head
-        left_matrix = None
+        head_matrix = None
         for idx, metaedge in enumerate(metapath):
             if verbose:
                 print("\tWorking on {}".format(idx))
             adj_mat = metaedge_to_adjacency_matrix(graph, metaedge)
             adj_mat = dwwc_step(adj_mat, damping, damping)
             adj_mat = csr_matrix(adj_mat)
-            # metanodes = metaedge.get_nodes()
-            if idx < first_appearance:  # haven't seen repeatednode yet
+            if idx < first_appearance:  # before repeated_node appears
                 if verbose:
-                    print("\t\tleft_matrix {}".format(idx))
-                if left_matrix is None:
+                    print("\t\thead_matrix {}".format(idx))
+                if head_matrix is None:
                     if verbose:
-                        print("\t\tleft_matrix {} initialize; shape adj {}".format(idx, adj_mat.shape))
-                    left_matrix = csr_matrix(adj_mat)
+                        print("\t\thead_matrix {} initialize; shape adj"
+                              " {}".format(idx, adj_mat.shape))
+                    head_matrix = adj_mat.copy()
                 else:
                     if verbose:
-                        print("\t\tleft_matrix {} multiply; shape left {} ; shape adj {}".format(idx, left_matrix.shape, adj_mat.shape))
+                        print("\t\thead_matrix {} multiply; shape left {} ;"
+                              "shape adj {}".format(idx, head_matrix.shape,
+                                                    adj_mat.shape))
                     if verbose:
-                        print("\t\tType of left_matrix before = {}".format(type(left_matrix)))
-                    left_matrix = left_matrix @ adj_mat
+                        print("\t\tType of head_matrix before = {}"
+                              "".format(type(head_matrix)))
+                    head_matrix = head_matrix @ adj_mat
             elif idx <= last_appearance:  # i.e. metanodes[0] = repeatednode
                 if verbose:
                     print("\t\tloop_matrix {}".format(idx))
                 if dwpc_matrix is None:
                     if verbose:
-                        print("\t\tloop_matrix {} initialize; shape adj {}".format(idx, adj_mat.shape))
-                    dwpc_matrix = csr_matrix(adj_mat)
+                        print("\t\tloop_matrix {} initialize; shape adj {}"
+                              "".format(idx, adj_mat.shape))
+                    dwpc_matrix = adj_mat.copy()
                 else:
                     if verbose:
-                        print("\t\tloop_matrix {} multiply; shape loop {} ; shape adj {}".format(idx, dwpc_matrix.shape, adj_mat.shape))
-                        print("\t\tType of loop_matrix before = {}".format(type(dwpc_matrix)))
-                    adj_mat = csr_matrix(adj_mat)
+                        print("\t\tloop_matrix {} multiply; loop {} ; adj {}"
+                              "".format(idx, dwpc_matrix.shape, adj_mat.shape))
+                        print("\t\tType of loop_matrix before = {}"
+                              "".format(type(dwpc_matrix)))
                     dwpc_matrix = dwpc_matrix @ adj_mat
+
                     # if endpoints are same type, subtract diagonal after mult
                     if metaedge.target == repeated_node[0]:
                         if verbose:
                             print("\tsubtracting diag {} ".format(idx))
-                            # np.diag(diagonals[0], offsets[0])
-                            # scipy version here
-                            print(dwpc_matrix.diagonal().shape)
-                        dwpc_matrix -= scipy.sparse.diags( [dwpc_matrix.diagonal()], [0] )
-            else:
+                        dwpc_matrix -= \
+                            scipy.sparse.diags([dwpc_matrix.diagonal(
+                                ).astype(float)], [0])
+            else:  # covers the tail cases
                 if verbose:
                     print("\t\ttail_matrix {}".format(idx))
                 dwpc_matrix = dwpc_matrix @ adj_mat
 
-    return left_matrix @ dwpc_matrix
+    return head_matrix @ dwpc_matrix
