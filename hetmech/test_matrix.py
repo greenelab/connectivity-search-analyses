79a60104e3fc031a159e27d1e2822a637b8c87cc
zietzm <michael.zietz@gmail.com>
2017-05-25 16:55:42 -0500

    Hard-code test data in test_matrix

--- hetmech/test_matrix.py
+++ hetmech/test_matrix.py
@@ -1,51 +1,28 @@
-import numpy
-from scipy import sparse
 import hetio.readwrite
-import hetio.hetnet
+from .matrix import metaedge_to_adjacency_matrix
 
-from .matrix import get_node_to_position, metaedge_to_adjacency_matrix
+'''
+Test the functionality of metaedge_to_adjacency_matrix in
+generating sparse matrices vs numpy arrays. Uses same test
+data as in test_degree_weight.py Figure 2D of Himmelstein &
+Baranzini (2015) PLOS Comp Bio.
+https://doi.org/10.1371/journal.pcbi.1004259.g002
+'''
+
+url = 'https://github.com/dhimmel/hetio/raw/{}/{}'.format(
+    '9dc747b8fc4e23ef3437829ffde4d047f2e1bdde',
+    'test/data/disease-gene-example-graph.json',
+)
+graph = hetio.readwrite.read_graph(url)
 
 
 def test_sparse_matrix():
-    '''
-    Test the functionality of metaedge_to_adjacency_matrix in
-    generating sparse matrices vs numpy arrays. Uses same test
-    data as in test_degree_weight.py Figure 2D of Himmelstein &
-    Baranzini (2015) PLOS Comp Bio.
-    https://doi.org/10.1371/journal.pcbi.1004259.g002
-    '''
+    mat = metaedge_to_adjacency_matrix(graph, 'GiG')
+    assert all([mat[2][0, 4], mat[2][0, 2], mat[2][6, 2], mat[2][2, 0]])
+    assert not any([mat[2][0, 0], mat[2][0, 1], mat[2][6, 0], mat[2][6, 1]])
 
-    url = 'https://github.com/dhimmel/hetio/raw/{}/{}'.format(
-        '9dc747b8fc4e23ef3437829ffde4d047f2e1bdde',
-        'test/data/disease-gene-example-graph.json',
-    )
-    graph = hetio.readwrite.read_graph(url)
 
-    def inef_metaedge_to_adjacency_matrix(graph, metaedge, dtype=numpy.bool_):
-        """
-        Returns a sparse matrix where source nodes are rows and target
-        nodes are columns by converting a numpy array.
-        """
-        if not isinstance(metaedge, hetio.hetnet.MetaEdge):
-            # metaedge is an abbreviation
-            metaedge = graph.metagraph.metapath_from_abbrev(metaedge)[0]
-        source_nodes = list(get_node_to_position(graph, metaedge.source))
-        target_node_to_position = get_node_to_position(graph, metaedge.target)
-        shape = len(source_nodes), len(target_node_to_position)
-        adjacency_matrix = numpy.zeros(shape, dtype=dtype)
-        for i, source_node in enumerate(source_nodes):
-            for edge in source_node.edges[metaedge]:
-                j = target_node_to_position[edge.target]
-                adjacency_matrix[i, j] = 1
-        # Inefficient but effective conversion
-        adjacency_matrix = sparse.csr_matrix(adjacency_matrix)
-        row_names = [node.identifier for node in source_nodes]
-        column_names = [node.identifier for node in target_node_to_position]
-        return row_names, column_names, adjacency_matrix
-
-    mat_ineff = inef_metaedge_to_adjacency_matrix(graph, 'GiG')
-    mat_eff = metaedge_to_adjacency_matrix(graph, 'GiG')
-
-    assert mat_ineff[2][1, 2] is mat_eff[2][1, 2]
-    assert mat_ineff[2][4, 2] is mat_eff[2][4, 2]
-    assert all([mat_eff[2][6, 2], mat_eff[2][0, 4], mat_eff[2][2, 0]])
+def test_sparse_matrix_2():
+    mat = metaedge_to_adjacency_matrix(graph, 'GaD')
+    assert all([mat[2][0, 1], mat[2][1, 1], mat[2][2, 0], mat[2][3, 1]])
+    assert not any([mat[2][0, 0], mat[2][1, 0], mat[2][2, 1], mat[2][3, 0]])
