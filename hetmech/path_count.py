3575e2c1cf6234a5c22c7369c7944c59ba329798
Kyle Kloster <kyle.kloster@gmail.com>
2017-05-31 09:50:28 -0400

    updating path_count for new metaedge_to_adjacency implementation; cleaning up code

--- hetmech/path_count.py
+++ hetmech/path_count.py
@@ -39,20 +39,14 @@ def dwpc(graph, metapath, damping=1.0, verbose=False):
     repeated_node = list(filter(lambda x: nodetype_sequence[x] >= 2,
                          nodetype_sequence))
     if len(repeated_node) > 1:
-        if verbose:
-            print("Input metapath repeats more than one nodetype")
         raise NotImplementedError("Metapath repeats more than one metanode")
 
     # Compute DWPC
     elif len(repeated_node) == 0:  # case 1: no repeated node types
-        if verbose:
-            print("Input metapath has no repeated nodetypes")
-        # Now perform multiplications
         return dwwc(graph, metapath, damping)
 
     else:  # case 2: one node type repeated
         # Determine start/endpoints for head, loop, tail
-        dwpc_matrix = None
         if verbose:
             print("Input metapath repeats exactly one nodetype")
             print("Repeated node list: {}, type {}"
@@ -72,57 +66,30 @@ def dwpc(graph, metapath, damping=1.0, verbose=False):
                             [first_appearance, last_appearance]))
 
         # Iterate over each meta-edge adjacency matrix
+        dwpc_matrix = None
         head_matrix = None
         for idx, metaedge in enumerate(metapath):
             if verbose:
                 print("\tWorking on {}".format(idx))
-            adj_mat = metaedge_to_adjacency_matrix(graph, metaedge)
-            adj_mat = dwwc_step(adj_mat, damping, damping)
+            _, _, adj_mat = metaedge_to_adjacency_matrix(graph, metaedge)
+            adj_mat = dwwc_step(adj_mat, damping, damping, False)
             adj_mat = csr_matrix(adj_mat)
             if idx < first_appearance:  # before repeated_node appears
-                if verbose:
-                    print("\t\thead_matrix {}".format(idx))
                 if head_matrix is None:
-                    if verbose:
-                        print("\t\thead_matrix {} initialize; shape adj"
-                              " {}".format(idx, adj_mat.shape))
                     head_matrix = adj_mat.copy()
                 else:
-                    if verbose:
-                        print("\t\thead_matrix {} multiply; shape left {} ;"
-                              "shape adj {}".format(idx, head_matrix.shape,
-                                                    adj_mat.shape))
-                    if verbose:
-                        print("\t\tType of head_matrix before = {}"
-                              "".format(type(head_matrix)))
                     head_matrix = head_matrix @ adj_mat
             elif idx <= last_appearance:  # i.e. metanodes[0] = repeatednode
-                if verbose:
-                    print("\t\tloop_matrix {}".format(idx))
                 if dwpc_matrix is None:
-                    if verbose:
-                        print("\t\tloop_matrix {} initialize; shape adj {}"
-                              "".format(idx, adj_mat.shape))
                     dwpc_matrix = adj_mat.copy()
                 else:
-                    if verbose:
-                        print("\t\tloop_matrix {} multiply; loop {} ; adj {}"
-                              "".format(idx, dwpc_matrix.shape, adj_mat.shape))
-                        print("\t\tType of loop_matrix before = {}"
-                              "".format(type(dwpc_matrix)))
                     dwpc_matrix = dwpc_matrix @ adj_mat
-
                     # if endpoints are same type, subtract diagonal after mult
                     if str(metaedge.target) == repeated_node[0]:
-                        if verbose:
-                            print("\tsubtracting diag {} ".format(idx))
                         dwpc_matrix -= \
                             scipy.sparse.diags([dwpc_matrix.diagonal(
                                 ).astype(float)], [0])
             else:  # covers the tail cases
-                if verbose:
-                    print("\t\ttail_matrix {} multiply; shape adj {}"
-                          "".format(idx, adj_mat.shape))
                 dwpc_matrix = dwpc_matrix @ adj_mat
 
         return head_matrix @ dwpc_matrix
