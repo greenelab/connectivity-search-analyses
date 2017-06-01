f52e3398844786189ed7464b5b4478ca4fdb4788
zietzm <michael.zietz@gmail.com>
2017-06-01 17:28:02 -0400

    Parametrize data type in test_matrix

--- hetmech/test_matrix.py
+++ hetmech/test_matrix.py
@@ -13,7 +13,7 @@ gig_adj = numpy.array([[0, 0, 1, 0, 1, 0, 0],
                        [0, 0, 1, 0, 0, 0, 0],
                        [1, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 1, 0, 0, 0, 0]])
+                       [0, 0, 1, 0, 0, 0, 0]], dtype=numpy.bool_)
 gig_adj_csc = sparse.csc_matrix(gig_adj)
 gad_rows = ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1']
 gad_cols = ["Crohn's Disease", 'Multiple Sclerosis']
@@ -23,35 +23,35 @@ gad_adj = numpy.array([[0, 1],
                        [0, 1],
                        [0, 0],
                        [1, 1],
-                       [0, 0]])
-gad_adj_csc = sparse.csc_matrix(gad_adj)
+                       [0, 0]], dtype=numpy.float64)
+gad_adj_csc = sparse.csc_matrix(gad_adj, dtype=numpy.bool_)
 dlt_rows = ["Crohn's Disease", 'Multiple Sclerosis']
 dlt_cols = ['Leukocyte', 'Lung']
 dlt_adj = numpy.array([[0, 0],
-                       [1, 0]])
+                       [1, 0]], dtype=numpy.bool_)
 dlt_adj_csc = sparse.csc_matrix(dlt_adj)
 tld_rows = ['Leukocyte', 'Lung']
 tld_cols = ["Crohn's Disease", 'Multiple Sclerosis']
 tld_adj = numpy.array([[0, 1],
                        [0, 0]])
-tld_adj_csc = sparse.csc_matrix(tld_adj)
+tld_adj_csc = sparse.csc_matrix(tld_adj, dtype=numpy.int64)
 
 
-@pytest.mark.parametrize("test_edge,exp_row,exp_col,exp_adj,mat_type", [
-    ('GiG', gig_rows, gig_cols, gig_adj, numpy.array),
-    ('GiG', gig_rows, gig_cols, gig_adj_csc, sparse.csc_matrix),
+@pytest.mark.parametrize("test_edge,exp_row,exp_col,exp_adj,mat_type,dtype", [
+    ('GiG', gig_rows, gig_cols, gig_adj, numpy.array, numpy.bool_),
+    ('GiG', gig_rows, gig_cols, gig_adj_csc, sparse.csc_matrix, numpy.int32),
 
-    ('GaD', gad_rows, gad_cols, gad_adj, numpy.array),
-    ('GaD', gad_rows, gad_cols, gad_adj_csc, sparse.csc_matrix),
+    ('GaD', gad_rows, gad_cols, gad_adj, numpy.array, numpy.float64),
+    ('GaD', gad_rows, gad_cols, gad_adj_csc, sparse.csc_matrix, numpy.bool_),
 
-    ('DlT', dlt_rows, dlt_cols, dlt_adj, numpy.array),
-    ('DlT', dlt_rows, dlt_cols, dlt_adj_csc, sparse.csc_matrix),
+    ('DlT', dlt_rows, dlt_cols, dlt_adj, numpy.array, numpy.bool_),
+    ('DlT', dlt_rows, dlt_cols, dlt_adj_csc, sparse.csc_matrix, numpy.int32),
 
-    ('TlD', tld_rows, tld_cols, tld_adj, numpy.array),
-    ('TlD', tld_rows, tld_cols, tld_adj_csc, sparse.csc_matrix)
+    ('TlD', tld_rows, tld_cols, tld_adj, numpy.array, numpy.int32),
+    ('TlD', tld_rows, tld_cols, tld_adj_csc, sparse.csc_matrix, numpy.int64)
 ])
 def test_metaedge_to_adjacency_matrix(test_edge, exp_row, exp_col, exp_adj,
-                                      mat_type):
+                                      mat_type, dtype):
     """
     Test the functionality of metaedge_to_adjacency_matrix in generating
     numpy arrays. Uses same test data as in test_degree_weight.py
@@ -64,10 +64,11 @@ def test_metaedge_to_adjacency_matrix(test_edge, exp_row, exp_col, exp_adj,
     )
     graph = hetio.readwrite.read_graph(url)
     row_names, col_names, adj_mat = \
-        metaedge_to_adjacency_matrix(graph, test_edge, matrix_type=mat_type)
+        metaedge_to_adjacency_matrix(graph, test_edge, matrix_type=mat_type,
+                                     dtype=dtype)
 
     assert numpy.array_equal(row_names, exp_row)
     assert numpy.array_equal(col_names, exp_col)
     assert not type((exp_adj != adj_mat)).max(exp_adj != adj_mat)
-    # assert adj_mat.dtype == dtype
+    assert adj_mat.dtype == dtype
     assert isinstance(adj_mat, type(exp_adj))
