ef9d8dc47f95faa0470a9f583e8e57df157471dd
zietzm <michael.zietz@gmail.com>
2017-06-02 16:14:53 -0400

    Simplified test dictionary. Added numpy.float64 as a tested dtype. Simplified array comparison.

--- hetmech/test_matrix.py
+++ hetmech/test_matrix.py
@@ -6,18 +6,11 @@ from scipy import sparse
 from .matrix import metaedge_to_adjacency_matrix
 
 
-def get_arrays(edge, mat_type, dtype):  # Define a dictionary
-    rows_dict = {
-        'GiG': ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1'],
-        'GaD': ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1'],
-        'DlT': ["Crohn's Disease", 'Multiple Sclerosis'],
-        'TlD': ['Leukocyte', 'Lung']
-    }
-    cols_dict = {
-        'GiG': ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1'],
-        'GaD': ["Crohn's Disease", 'Multiple Sclerosis'],
-        'DlT': ['Leukocyte', 'Lung'],
-        'TlD': ["Crohn's Disease", 'Multiple Sclerosis']
+def get_arrays(edge, mat_type, dtype):
+    node_dict = {
+        'G': ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1'],
+        'D': ["Crohn's Disease", 'Multiple Sclerosis'],
+        'T': ['Leukocyte', 'Lung']
     }
     adj_dict = {
         'GiG': [[0, 0, 1, 0, 1, 0, 0],
@@ -39,14 +32,14 @@ def get_arrays(edge, mat_type, dtype):  # Define a dictionary
         'TlD': [[0, 1],
                 [0, 0]]
     }
-    return rows_dict[edge], cols_dict[edge], mat_type(adj_dict[edge],
-                                                      dtype=dtype)
+    return node_dict[edge[0]], node_dict[edge[-1]], mat_type(adj_dict[edge],
+                                                             dtype=dtype)
 
 
 @pytest.mark.parametrize("test_edge", ['GiG', 'GaD', 'DlT', 'TlD'])
 @pytest.mark.parametrize("mat_type", [numpy.array, sparse.csc_matrix,
                                       sparse.csr_matrix, numpy.matrix])
-@pytest.mark.parametrize("dtype", [numpy.bool_, numpy.int64, numpy.int32])
+@pytest.mark.parametrize("dtype", [numpy.bool_, numpy.int64, numpy.float64])
 def test_metaedge_to_adjacency_matrix(test_edge, mat_type, dtype):
     """
     Test the functionality of metaedge_to_adjacency_matrix in generating
@@ -66,9 +59,7 @@ def test_metaedge_to_adjacency_matrix(test_edge, mat_type, dtype):
 
     assert row_names == exp_row
     assert col_names == exp_col
-    assert type(adj_mat) == type(exp_adj)  # Arrays same type
-    assert not sparse.csc_matrix(adj_mat != exp_adj).nnz  # Arrays equal
+    assert type(adj_mat) == type(exp_adj)
     assert adj_mat.dtype == dtype
-    assert isinstance(
-        adj_mat, mat_type if mat_type != numpy.array else numpy.ndarray
-    )
+    assert adj_mat.shape == exp_adj.shape
+    assert (adj_mat != exp_adj).sum() == 0
