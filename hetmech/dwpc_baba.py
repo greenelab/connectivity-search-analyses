7c0eec50dbc392c4715b0739a9419151762e7f6e
zietzm <michael.zietz@gmail.com>
2017-07-24 12:18:22 -0400

    Add BABA functionality and tests for it

---
+++ hetmech/dwpc_baba.py
@@ -0,0 +1,75 @@
+import numpy
+
+from .matrix import metaedge_to_adjacency_matrix, normalize
+
+
+def index_to_baba(index, adjacency):
+    """
+    Takes an index and adjacency matrix and outputs an array of the child
+    nodes a distance away. The function finds the children for one source
+    node. If a weighted adjacency matrix is provided, this function computes
+    the degree-weighted path count (DWPC). An unweighted matrix returns
+    simply the path count.
+
+    Parameters
+    ----------
+    index : int
+        row index of the source node
+    adjacency : numpy.ndarray
+        adjacency matrix corresponding to the metaedge
+
+    Returns
+    -------
+    numpy.ndarray
+        1 dimensional array corresponding to the (PC/DWPC) of target nodes
+    """
+    # Create a vector for the given index
+    b0 = numpy.zeros(adjacency.shape[0])
+    b0[index] = 1
+
+    # Find the child nodes and add the previous node's index at the
+    #  zero position, elongating the vector by one.
+    a0 = [numpy.insert(i, 0, index) for i in numpy.diag(b0 @ adjacency) if
+          i.any()]
+
+    b1 = []
+    for i in a0:
+        # Keep the nonzero element's index for history purposes
+        ind = (i[1:] != 0).tolist().index(1)
+        # Find the next set of child nodes (ignore history element)
+        child = i[1:] @ adjacency.T
+        # Set the element corresponding to the previous node equal to zero
+        child[int(i[0])] = 0
+        # Split the list of child nodes and add a history position again
+        nodes = [numpy.insert(i, 0, ind) for i in numpy.diag(child) if i.any()]
+        b1 += nodes
+    # Roughly repeat the procedure
+    a1 = numpy.zeros(adjacency.shape[1])
+    for i in b1:
+        child = i[1:] @ adjacency
+        child[int(i[0])] = 0
+        a1 += child
+    return a1
+
+
+def dwpc_baba(graph, metapath, damping=0.5):
+    """
+    Computes the degree-weighted path count for overlapping metanode
+    repeats of the form B-A-B-A. Note that this does NOT yet support
+    B-A-C-B-A, or any sort of metapath wherein there are metanodes
+    within the overlapping region.
+    """
+    metaedges = metapath.edges
+    assert len(set(metaedges)) == 2
+
+    # Get and normalize the adjacency matrix
+    row, col, adjacency = metaedge_to_adjacency_matrix(
+        graph, metaedges[0], dtype=numpy.float64, sparse_threshold=0)
+    rowsums = adjacency.sum(axis=1)
+    colsums = adjacency.sum(axis=0)
+    adjacency = normalize(adjacency, rowsums, 'rows', damping)
+    adjacency = normalize(adjacency, colsums, 'columns', damping)
+
+    ret = [index_to_baba(i, adjacency) for i in range(adjacency.shape[0])]
+    ret = numpy.array(ret, dtype=numpy.float64)
+    return row, col, ret
---
+++ hetmech/test_dwpc_baba.py
@@ -0,0 +1,65 @@
+import hetio.readwrite
+import numpy
+import pytest
+
+from .dwpc_baba import dwpc_baba
+
+
+def get_matrices(metapath):
+    node_dict = {
+        'G': ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1'],
+        'D': ["Crohn's Disease", 'Multiple Sclerosis'],
+        'T': ['Leukocyte', 'Lung']
+    }
+    edge_dict = {
+        'GaD': [[0.08838835, 0],
+                [0.08838835, 0],
+                [0, 0.125],
+                [0.08838835, 0],
+                [0, 0],
+                [0, 0],
+                [0, 0]],
+        'DlT': [[0, 0],
+                [0, 0]],
+        'TeG': [[0, 0],
+                [0, 0],
+                [0, 0],
+                [0, 0],
+                [0, 0],
+                [0, 0],
+                [0, 0]]
+    }
+    mat_dict = {
+        'GaDaGaD': ('GaD', 0),
+        'DaGaDaG': ('GaD', 1),
+        'DlTlDlT': ('DlT', 0),
+        'TlDlTlD': ('DlT', 0),
+        'GeTeGeT': ('TeG', 0),
+        'TeGeTeG': ('TeG', 1)
+    }
+    first = node_dict[metapath[0]]
+    last = node_dict[metapath[-1]]
+    edge = mat_dict[metapath]
+    adj = numpy.array(edge_dict[edge[0]], dtype=numpy.float64)
+    if edge[1]:
+        adj = adj.transpose()
+    return first, last, adj
+
+
+@pytest.mark.parametrize('m_path', ('GaDaGaD', 'DaGaDaG', 'DlTlDlT',
+                                    'TlDlTlD', 'GeTeGeT', 'TeGeTeG'))
+def test_dwpc_baba(m_path):
+    url = 'https://github.com/dhimmel/hetio/raw/{}/{}'.format(
+        '9dc747b8fc4e23ef3437829ffde4d047f2e1bdde',
+        'test/data/disease-gene-example-graph.json',
+    )
+    graph = hetio.readwrite.read_graph(url)
+    metagraph = graph.metagraph
+    metapath = metagraph.metapath_from_abbrev(m_path)
+
+    row_sol, col_sol, adj_sol = get_matrices(m_path)
+    row, col, dwpc = dwpc_baba(graph, metapath, damping=0.5)
+
+    assert row_sol == row
+    assert col_sol == col
+    assert pytest.approx(adj_sol, dwpc)
