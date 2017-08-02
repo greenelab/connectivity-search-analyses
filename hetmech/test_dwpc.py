820569f495bb96767a8390a9deb0cbee018471c9
zietzm <michael.zietz@gmail.com>
2017-08-02 15:10:13 -0400

    Add tests for dwpc_general_case

--- hetmech/test_dwpc.py
+++ hetmech/test_dwpc.py
@@ -129,6 +129,60 @@ def test_dwpc_baba(m_path):
     assert numpy.max(adj_sol - dwpc) == pytest.approx(0, abs=1e-8)
 
 
+def get_general_solutions(length):
+    genes = ['CXCR4', 'IL2RA', 'IRF1', 'IRF8', 'ITCH', 'STAT3', 'SUMO1']
+    mat_dict = {
+        0: [[0, 0, 0.35355339, 0, 0.70710678, 0, 0],
+            [0, 0, 0.5, 0, 0, 0, 0],
+            [0.35355339, 0.5, 0, 0.5, 0, 0, 0.5],
+            [0, 0, 0.5, 0, 0, 0, 0],
+            [0.70710678, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0.5, 0, 0, 0, 0]],
+        1: [[0, 0.1767767, 0, 0.1767767, 0, 0, 0.1767767],
+            [0.1767767, 0, 0, 0.25, 0, 0, 0.25],
+            [0, 0, 0, 0, 0.25, 0, 0],
+            [0.1767767, 0.25, 0, 0, 0, 0, 0.25],
+            [0, 0, 0.25, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0.1767767, 0.25, 0, 0.25, 0, 0, 0]],
+        2: [[0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0.125, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0.125, 0, 0],
+            [0, 0.125, 0, 0.125, 0, 0, 0.125],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0.125, 0, 0]],
+        3: numpy.zeros((7, 7)),
+        4: numpy.zeros((7, 7)),
+        5: numpy.zeros((7, 7))
+    }
+    return genes, genes, mat_dict[length]
+
+
+@pytest.mark.parametrize('length', list(range(6)))
+def test_dwpc_general_case(length):
+    """
+    Test the functionality of dwpc_same_metanode to find DWPC
+    within a metapath (segment) of metanode and metaedge repeats.
+    """
+    url = 'https://github.com/dhimmel/hetio/raw/{}/{}'.format(
+        '9dc747b8fc4e23ef3437829ffde4d047f2e1bdde',
+        'test/data/disease-gene-example-graph.json',
+    )
+    graph = hetio.readwrite.read_graph(url)
+    metagraph = graph.metagraph
+    m_path = 'GiG' + length*'iG'
+    metapath = metagraph.metapath_from_abbrev(m_path)
+    rows, cols, dwpc_mat = dwpc_general_case(graph, metapath, damping=0.5)
+    exp_row, exp_col, exp_dwpc = get_general_solutions(length)
+
+    # Test matrix, row, and column label output
+    assert abs(dwpc_mat - exp_dwpc).sum() == pytest.approx(0, abs=1e-7)
+    assert rows == exp_row
+    assert cols == exp_col
+
+
 @pytest.mark.parametrize('metapath,solution', [
     ('GiG', 'short_repeat'),
     ('GiGiGiG', 'long_repeat'),
