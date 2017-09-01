c171417b3ba3b3ef69ba30eee9dcb616080bf062
zietzm <michael.zietz@gmail.com>
2017-08-31 22:57:04 -0400

    Update hetionet import

--- hetmech/test_short_repeat_error.py
+++ hetmech/test_short_repeat_error.py
@@ -3,12 +3,6 @@ import pytest
 
 from .degree_weight import dwpc
 
-commit = '59c448fd912555f84b9822b4f49b431b696aea15'
-
-url = (f'https://github.com/dhimmel/hetionet/raw/{commit}/hetnet/json/'
-       f'hetionet-v1.0.json.bz2')
-graph = hetio.readwrite.read_graph(url)
-
 
 @pytest.mark.parametrize('error_compound,error_disease,metapath,cypher', [
     ('DB00860', 'DOID:3083', 'CpDrDdGdD', 0.012299),
@@ -25,6 +19,12 @@ graph = hetio.readwrite.read_graph(url)
     ('DB00313', 'DOID:1826', 'CtDrDaGaD', 0.000395)
 ])
 def test_short_error(error_compound, error_disease, metapath, cypher):
+    commit = '59c448fd912555f84b9822b4f49b431b696aea15'
+
+    url = (f'https://github.com/dhimmel/hetionet/raw/{commit}/hetnet/json/'
+           f'hetionet-v1.0.json.bz2')
+    graph = hetio.readwrite.read_graph(url)
+
     compounds, diseases, dwpc_matrix, seconds = dwpc(
         graph, graph.metagraph.metapath_from_abbrev(metapath), damping=0.4,
         sparse_threshold=1)
