54d9848807b67e20b1dc906b51ee9e866ff0f163
zietzm <michael.zietz@gmail.com>
2018-04-02 14:09:16 -0400

    Add hetio requirement

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+from pip.req import parse_requirements
+install_reqs = parse_requirements('environment.yml')
+
 setup(name='hetmech',
       description='A search engine for hetnets',
       long_description='Matrix implementations of path-count-based measures',
       url='https://github.com/greenelab/hetmech',
       license='BSD 3-Clause License',
       packages=['hetmech'],
+      install_requires=['hetio']
       )
