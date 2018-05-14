6736e1a8d9617f674d4ed205aadf79a84389f2a6
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-05-14 11:29:58 -0400

    HetMat: make metagraph a property

--- hetmech/hetmat.py
+++ hetmech/hetmat.py
@@ -1,23 +1,28 @@
+import functools
 import pathlib
+
 import hetio.hetnet
 import hetio.readwrite
 import hetio.matrix
 
-def hetmat_from_graph(graph):
+
+def hetmat_from_graph(graph, path):
     """
     Create a hetmat.HetMat from a hetio.hetnet.Graph.
     """
     assert isinstance(graph, hetio.hetnet.Graph)
-    metagraph = graph.metagraph
+    hetmat = HetMat(path, initialize=True)
+    hetmat.metagraph = graph.metagraph
+    metagraph = hetmat.metagraph
     metaedges = list(metagraph.get_edges(exclude_inverts=True))
     for metaedge in metaedges:
         matrix = hetio.matrix.metaedge_to_adjacency_matrix(metaedge)
-    hetmat = HetMat(metagraph)
+    return hetmat
 
 
 class HetMat:
 
-    def __init__(self, directory, initialize=False, metagraph=None):
+    def __init__(self, directory, initialize=False):
         """
         Initialize a HetMat with its MetaGraph.
         """
@@ -26,18 +31,17 @@ class HetMat:
         self.nodes_directory = directory.joinpath('nodes')
         self.edges_directory = directory.joinpath('edges')
         if initialize:
-            assert metagraph is not None
-            self.metagraph = metagraph
             self.initialize()
-        self.metagraph = hetio.readwrite.read_metagraph(self.metagraph_path)
         return self
 
     def initialize(self):
         """
-        Initialize a directory.
+        Initialize the directory structure. This function is intended to be
+        called when creating new HetMat instance on disk.
         """
         # Create directories
         directories = [
+            self.directory,
             self.metagraph_path,
             self.nodes_directory,
             self.edges_directory,
@@ -45,5 +49,22 @@ class HetMat:
         for directory in directories:
             if not directory.isdir():
                 directory.mkdir()
-        # Write metagraph
-        hetio.readwrite.write_metagraph(self.metagraph_path)
+
+    @property
+    @functools.lru_cache()
+    def metagraph(self):
+        """
+        HetMat.metagraph is a cached property. Hence reading the metagraph from
+        disk should only occur once, the first time the metagraph property is
+        accessed. See https://stackoverflow.com/a/19979379/4651668. If this
+        method has issues, consider using cached_property from
+        https://github.com/pydanny/cached-property.
+        """
+        return hetio.readwrite.read_metagraph(self.metagraph_path)
+
+    @metagraph.setter
+    def metagraph(self, metagraph):
+        """
+        Set the metagraph property by writing the metagraph to disk.
+        """
+        hetio.readwrite.write_metagraph(metagraph, self.metagraph_path)
