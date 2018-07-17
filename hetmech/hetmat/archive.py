e8c6e282320c30146fbbb6ed3a1bc20184775bb4
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-07-17 11:29:27 -0400

    create_archive: enable splitting zip output
    Co-authored-by: Michael Zietz <michael.zietz@gmail.com>

--- hetmech/hetmat/archive.py
+++ hetmech/hetmat/archive.py
@@ -39,23 +39,42 @@ def create_archive_by_globs(
         source_paths -= set(root_directory.glob(glob))
     source_paths = [path.relative_to(root_directory) for path in source_paths]
     source_paths.extend(map(pathlib.Path, include_paths))
-    create_archive(destination_path, root_directory, source_paths, **kwargs)
+    return create_archive(destination_path, root_directory, source_paths, **kwargs)
 
 
-def create_archive(destination_path, root_directory, source_paths, zip_mode='x', compression=zipfile.ZIP_LZMA):
+def create_archive(destination_path, root_directory, source_paths, zip_mode='x', compression=zipfile.ZIP_LZMA, split_size=None):
     """
     Create a zip archive of the source paths at the destination path.
     source_paths as paths relative to the hetmat root directory.
+    split_size is the max zip file size in GB before spliting the zip into
+    multiple parts.
     """
     root_directory = pathlib.Path(root_directory)
-    destination_path = pathlib.Path(destination_path)
     assert zip_mode in {'w', 'x', 'a'}
-    zip_file = zipfile.ZipFile(destination_path, mode=zip_mode, compression=compression)
     source_paths = sorted(set(map(str, source_paths)))
+    destination_path = pathlib.Path(destination_path)
+    if split_size is None:
+        zip_path = destination_path
+    else:
+        zip_path_formatter = f'{destination_path.stem}-{{:04d}}{destination_path.suffix}'.format
+        split_num = 0
+        zip_path = zip_path_formatter(split_num)
+    zip_paths = [zip_path]
+    zip_file = zipfile.ZipFile(zip_path, mode=zip_mode, compression=compression)
     for source_path in source_paths:
         source_fs_path = root_directory.joinpath(source_path)
+        if split_size is not None:
+            fs_size_gb = source_fs_path.stat().st_size / 1e9
+            zip_size_gb = zip_path.stat().st_size / 1e9
+            if zip_size_gb + fs_size_gb > split_size:
+                zip_file.close()
+                split_num += 1
+                zip_path = zip_path_formatter(split_num)
+                zip_paths.append(zip_path)
+                zip_file = zipfile.ZipFile(zip_path, mode=zip_mode, compression=compression)
         zip_file.write(source_fs_path, source_path)
     zip_file.close()
+    return zip_paths
 
 
 def load_archive(archive_path, destination_dir):
