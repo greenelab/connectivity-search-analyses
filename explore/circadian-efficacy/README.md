53fc419260911a2ceb9bad78f7bccf7febf3728a
yhao-compbio <Leo.Hao@outlook.com>
2019-06-05 11:44:22 -0400

    Calculate edge circadian score using two datasets

--- explore/circadian-efficacy/README.md
+++ explore/circadian-efficacy/README.md
@@ -29,9 +29,11 @@ Evaluating unsupervised DWPC p-values and path contributions to predict whether 
 + [`data/circa_db_mapped.tsv`](data/circa_db_mapped.tsv) is a processed version of [`downloads/aat8806_Data_file_S1.xlsx`](downloads/aat8806_Data_file_S1.xlsx).
   It organized circadian scores by gene, added median tissue-specific expression value of each gene from GTEx.
 
-+ [`data/ruben_hetionet_edges.txt`](data/ruben_hetionet_edges.txt) and [`data/drugbank_hetionet_edges.txt`](data/drugbank_hetionet_edges.txt) contain extracted drug-disease paths from hetionet.
++ [`data/rephetio_significant_metapaths.tsv`](data/rephetio_significant_metapaths.tsv) contains 67 metapaths that are used for calculating the edge circadian score. These metapaths were extracted from [Project Rephetio](https://het.io/repurpose/metapaths.html). Metapaths with delta(AUROC)>0 and -log(P)>1.30 were considered. 
 
-+ [`data/hetionet_edges_mean_circadian_score.tsv`](data/hetionet_edges_mean_circadian_score.tsv), [`data/hetionet_edges_weightedmean_circadian_score.tsv`](data/hetionet_edges_weightedmean_circadian_score.tsv), and [`data/hetionet_edges_max_circadian_score.tsv`](data/hetionet_edges_max_circadian_score.tsv) contain calculated scores of extracted drug-diseasse paths using mean, weighted mean, and maximum scoring method, respectively.
++ [`data/HumCircMed2018v2_mapped_edge_circa_scores.tsv`](data/HumCircMed2018v2_mapped_edge_circa_scores.tsv) contains calculated calculate edge circadian scores for drug-disease pairs from Ruben et al dataset.
+
++ [`data/indication_edge_circa_scores.tsv`](data/indication_edge_circa_scores.tsv) contains calculated edge circadian scores for drug-disease pairs from indication dataset.
 
 # Notebook
 
@@ -39,10 +41,8 @@ Evaluating unsupervised DWPC p-values and path contributions to predict whether 
 
 + [`circa_db_data.ipynb`](circa_db_data.ipynb) contains the codes that show CircaDB score distribution by tissue, comparison between different scores, and comparison between different tissues.
 
-+ [`half_life_data.ipynb`](half_life_data.ipynb) contains the codes that show half-life distribution of all available drugs in DrugBank.
++ [`calculate_edge_circa_score_ruben.ipynb`](calculate_edge_circa_score_ruben.ipynb) contains the codes that calculate edge circadian scores for drug-disease pairs from Ruben et al dataset.
 
-+ [`extract_drug_to_disease_path.ipynb`](extract_drug_to_disease_path.ipynb) contains the codes that extract drug-disease paths from hetionet.
++ [`calculate_edge_circa_score_indication.ipynb`](calculate_edge_circa_score_indication.ipynb) contains the codes that calculate edge circadian scores for drug-disease pairs from indication dataset.
 
-+ [`score_drug_to_disease_path.ipynb`](score_drug_to_disease_path.ipynb) contains the codes that calculate scores for extracted paths based on genes in the paths.
-
-+ [`plot_drug_to_disease_score.ipynb`](plot_drug_to_disease_score.ipynb) contains the code that plot the score distribution of paths.
++ [`analyze_edge_circa_score.ipynb`](analyze_edge_circa_score.ipynb) contains the codes that analyze edge circadian scores of drug-disease pairs from Ruben et al dataset and indication dataset.
--- explore/circadian-efficacy/src/calculate_edge_circa_score.py
+++ explore/circadian-efficacy/src/calculate_edge_circa_score.py
@@ -5,20 +5,29 @@ import numpy as np
 import pandas as pd
 import requests
 
-def calculate_edge_circa_score(query_drug, query_disease, query_tissue, circa_df, amp_threshold = 0.1, fdr_threshold = 0.05):
+def calculate_edge_circa_score(query_drug, query_disease, query_tissue, circa_df, query_metapath = 'database', min_path_count = 1, amp_threshold = 0.1, fdr_threshold = 0.05):
 
 	'''
+	--Input  
 	query_drug: drugbank ID
 	query_disease: disease ontology ID
 	query_tissue: list of tissue name (must match names used in CircaDB)
 	circa_df: dataframe that contains CircaDB data ('data/circa_db_mapped.tsv')
+	query_metapath: list of metapaths that will be included in the calculation (default only  includes all metapaths) 
+	min_path_len: minimum number of paths bewteen query drug and query disease needed to generate a non-negative score (default 5) 
 	amp_threshold: amplitude threshold to define circadian gene (default 0.1)
 	fdr_threshold: FDR threshold to define circadian gene (default 0.05)
+	
+	-- Output:
+	float: Edge circadian score
+	String: reason why score cannot be calculated for query drug-disease pair (if any) 
+	float: number of metapaths between drug-disease pair
+	float: number of paths between drug-disease pair
 	'''
 
 	# number of query tissues
 	tissue_len = len(query_tissue)
-
+	
 	# get hetionet ID of the drug and the disease
 	drug_search = requests.get('https://search-api.het.io/v1/nodes/?search=' + query_drug).json()
 	disease_search = requests.get('https://search-api.het.io/v1/nodes/?search=' + query_disease).json()
@@ -26,97 +35,115 @@ def calculate_edge_circa_score(query_drug, query_disease, query_tissue, circa_df
 	# check whether the drug and the disease are in hetionet
 	if len(drug_search['results']) == 0 and len(disease_search['results']) == 0:
 		circa_edge_ratio = np.tile(float('nan'), tissue_len)
+		total_meta_count = total_path_count = float('nan')
 		note = 'query drug and disease not in hetionet'
 	
 	elif len(drug_search['results']) == 0:
 		circa_edge_ratio = np.tile(float('nan'), tissue_len)
+		total_meta_count = total_path_count = float('nan')
 		note = 'query drug not in hetionet'
     	
 	elif len(disease_search['results']) == 0:
 		circa_edge_ratio = np.tile(float('nan'), tissue_len)
+		total_meta_count = total_path_count = float('nan')
 		note = 'query disease not in hetionet'
 	
 	else: 
-		# get metapaths that connect the drug and the disease
+		# get drug ID, disease ID, metapath ID
 		drug_id = drug_search['results'][0]['id']
 		disease_id = disease_search['results'][0]['id']
-		meta_search = requests.get('http://search-api.het.io/v1/query-metapaths/?source=' + str(drug_id) + '&target=' + str(disease_id)).json()
-		
-		# check whether drug and disease are connected in hetionet
-		if len(meta_search['path_counts']) == 0:
-			circa_edge_ratio = np.tile(float('nan'), tissue_len)
-			note = 'query drug and disease not connected in hetionet'
-		
+		gene_metapath = []
+		# use metapaths in the database (sparse)
+		if query_metapath == 'database':
+			# get metapaths that connect the drug and the disease
+			meta_search = requests.get('http://search-api.het.io/v1/query-metapaths/?source=' + str(drug_id) + '&target=' + str(disease_id)).json()
+			if len(meta_search['path_counts']) > 0:
+				for i in range(0, len(meta_search['path_counts'])):
+					i_metapath = meta_search['path_counts'][i]['metapath_abbreviation']
+					if 'G' in i_metapath:
+						gene_metapath.append(i_metapath)
+		# use pre-defined metapaths
 		else:
-			# get all metapaths that contain gene and path counts
-			gene_metapath = []
-			gene_path_count = []
-			# get all metapaths that contain gene and path counts
-			for i in range(0, len(meta_search['path_counts'])):
-				i_metapath = meta_search['path_counts'][i]['metapath_abbreviation']
-				if 'G' in i_metapath:
-					gene_metapath.append(i_metapath)
-					gene_path_count.append(meta_search['path_counts'][i]['path_count'])
+			for i in range(0, len(query_metapath)):
+				if 'G' in query_metapath[i]:
+					gene_metapath.append(query_metapath[i])	
+						
+		# check whether drug and disease are connected by genes in hetionet
+		if len(gene_metapath) == 0:
+			circa_edge_ratio = np.tile(float('nan'), tissue_len)
+			total_meta_count = total_path_count = float('nan')
+			note = 'query drug and disease not connected by genes in hetionet'
 			
-			# check whether drug and disease are connected by genes in hetionet
-			if len(gene_metapath) == 0:
+		else:
+			total_edge_score = np.zeros(tissue_len)
+			circa_edge_score = np.zeros(tissue_len)
+			total_meta_count = 0
+			total_path_count = 0	
+			query_amp = [x + '_amp' for x in query_tissue]
+			query_fdr = [x + '_fdr' for x in query_tissue]
+			for j in range(0, len(gene_metapath)):
+				path_search = requests.get('http://search-api.het.io/v1/query-paths/?source=' + str(drug_id) + '&target=' + str(disease_id) + '&metapath=' + gene_metapath[j] + '&max-paths=-1').json()
+				if len(path_search['paths']) == 0:
+					continue
+
+				elif path_search['query']['metapath_adjusted_p_value'] == 1:
+					continue
+
+				else:
+					total_meta_count = total_meta_count + 1
+					# get max circadian score of the path
+					gene_path_loc = [int(index/2) for index, value in enumerate(gene_metapath[j]) if value == 'G']
+					for k in range(0, len(path_search['paths'])):
+
+						# get circadian score of the gene
+						path_gene_circa_amp = np.zeros(tissue_len)
+						path_gene_circa_fdr = np.zeros(tissue_len) + 1
+						path_gene_circa_count = 0
+						for l in range(0, len(gene_path_loc)):
+							# get entrez ID of genes in the path
+							loc_id = gene_path_loc[l]
+							gene_node_id = path_search['paths'][k]['node_ids'][loc_id]
+							gene_search =  requests.get('https://search-api.het.io/v1/nodes/' + str(gene_node_id)).json()
+							gene_id = int(gene_search['identifier'])
+							gene_circa_amp = circa_df[query_amp][circa_df['gene_id'] == gene_id]
+							gene_circa_fdr = circa_df[query_fdr][circa_df['gene_id'] == gene_id]
+
+							# check whether the gene is in circaDB
+							if len(gene_circa_amp) == 0:
+								continue
+			
+							else:
+								path_gene_circa_count = path_gene_circa_count + 1
+								for m in range(0, tissue_len):
+									tmp_amp = float(gene_circa_amp.iloc[:,m])
+									if tmp_amp > path_gene_circa_amp[m]:
+										path_gene_circa_amp[m] = tmp_amp
+										path_gene_circa_fdr[m] = float(gene_circa_fdr.iloc[:,m])
+								
+						if path_gene_circa_count > 0:
+							# get path importance score
+							total_path_count = total_path_count + 1
+							path_score = path_search['paths'][k]['score']
+							total_edge_score = total_edge_score + path_score
+							# check whether the gene is circadian
+							for n in range(0, tissue_len):
+								if path_gene_circa_amp[n] >= amp_threshold and path_gene_circa_fdr[n] < fdr_threshold:
+									circa_edge_score[n] = circa_edge_score[n] + path_score
+
+			# calculate proportion of circadian paths
+			if total_meta_count == 0:
 				circa_edge_ratio = np.tile(float('nan'), tissue_len)
+				total_path_count = float('nan')
 				note = 'query drug and disease not connected by genes in hetionet'
-			
 			else:
-				total_edge_score = np.zeros(tissue_len)
-				circa_edge_score = np.zeros(tissue_len)	
-				query_amp = [x + '_amp' for x in query_tissue]
-				query_fdr = [x + '_fdr' for x in query_tissue]
-				for j in range(0, len(gene_metapath)):
-					path_search = requests.get('http://search-api.het.io/v1/query-paths/?source=' + str(drug_id) + '&target=' + str(disease_id) + '&metapath=' + gene_metapath[j] + '&max-paths=' + str(gene_path_count[j])).json()
-					if len(path_search['paths']) == 0:
-						continue
+				if total_path_count == 0:
+					circa_edge_ratio = np.tile(float('nan'), tissue_len)
+					note = 'query drug and disease connected by genes not in CircaDB'
+				elif total_path_count < min_path_count:
+					circa_edge_ratio = np.tile(float('nan'), tissue_len)
+					note = 'query drug and disease connected by too few paths'
+				else:
+					circa_edge_ratio = circa_edge_score/total_edge_score
+					note = 'NaN'
 
-					else:
-						# get max circadian score of the path
-						gene_path_loc = [int(index/2) for index, value in enumerate(gene_metapath[j]) if value == 'G']
-						for k in range(0, len(path_search['paths'])):
-		
-							# get circadian score of the gene
-							path_gene_circa_amp = np.zeros(tissue_len)
-							path_gene_circa_fdr = np.zeros(tissue_len) + 1
-							path_gene_circa_count = 0
-							for l in range(0, len(gene_path_loc)):
-								# get entrez ID of genes in the path
-								loc_id = gene_path_loc[l]
-								gene_node_id = path_search['paths'][k]['node_ids'][loc_id]
-								gene_search =  requests.get('https://search-api.het.io/v1/nodes/' + str(gene_node_id)).json()
-								gene_id = int(gene_search['identifier'])
-								gene_circa_amp = circa_df[query_amp][circa_df['gene_id'] == gene_id]
-								gene_circa_fdr = circa_df[query_fdr][circa_df['gene_id'] == gene_id]
-
-								# check whether the gene is in circaDB
-								if len(gene_circa_amp) == 0:
-									continue
-								
-								else:
-									path_gene_circa_count = path_gene_circa_count + 1
-									for m in range(0, tissue_len):
-										tmp_amp = float(gene_circa_amp.iloc[:,m])
-										if tmp_amp > path_gene_circa_amp[m]:
-											path_gene_circa_amp[m] = tmp_amp
-											path_gene_circa_fdr[m] = float(gene_circa_fdr.iloc[:,m])
-								
-							if path_gene_circa_count > 0:
-								# get path importance score
-								path_score = path_search['paths'][k]['score']
-								total_edge_score = total_edge_score + path_score
-								# check whether the gene is circadian
-								for n in range(0, tissue_len):
-									if path_gene_circa_amp[n] >= amp_threshold and path_gene_circa_fdr[n] < fdr_threshold:
-										circa_edge_score[n] = circa_edge_score[n] + path_score
-
-				# calculate proportion of circadian paths
-				circa_edge_ratio = np.tile(float('nan'), tissue_len)
-				note = 'NaN'
-				for o in range(0, tissue_len):
-					if total_edge_score[o] > 0:
-						circa_edge_ratio[o] = circa_edge_score[o]/total_edge_score[o]
-                    
-	return circa_edge_ratio, note
+	return circa_edge_ratio, note, total_meta_count, total_path_count
